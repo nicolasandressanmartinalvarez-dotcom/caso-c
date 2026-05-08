de62d48b57beec32b3c38e272e36a3e69753568a
4ndres55-b <martin.hernandez.24.2006@gmail.com>
2026-05-08 19:36:52 -0400

    push

--- BackEnd/api-mascotas/src/main/java/com/sanosysalvos/mascotas_service/Repository/MascotaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sanosysalvos.mascotas_service.Repository;
-
-import com.sanosysalvos.mascotas_service.model.Mascota;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MascotaRepository extends JpaRepository<Mascota, Long> {
-}
