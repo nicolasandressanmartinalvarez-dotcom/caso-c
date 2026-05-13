c9609b44c81682aa64cf88d47cde91780824a969
4ndres55-b <martin.hernandez.24.2006@gmail.com>
2026-05-13 01:29:01 -0400

    se prepara para el comit definitivo, por ello se borra

--- BackEnd/api-notificaciones/src/main/java/com/api/notificaciones/repository/MensajeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.api.notificaciones.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.notificaciones.model.MensajePrueba;
-
-public interface MensajeRepo extends JpaRepository<MensajePrueba,Long> {
-    
-}
