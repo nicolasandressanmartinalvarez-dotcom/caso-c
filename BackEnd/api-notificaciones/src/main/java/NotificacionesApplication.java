45a223ea301c3e76b5d6a31b564516088eba5e5f
4ndres55-b <martin.hernandez.24.2006@gmail.com>
2026-06-02 11:36:38 -0400

    2-06-2026

--- BackEnd/api-notificaciones/src/main/java/NotificacionesApplication.java
+++ BackEnd/api-notificaciones/src/main/java/NotificacionesApplication.java
@@ -1,13 +1,10 @@
-package com.api.notificaciones;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class NotificacionesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NotificacionesApplication.class, args);
 	}
