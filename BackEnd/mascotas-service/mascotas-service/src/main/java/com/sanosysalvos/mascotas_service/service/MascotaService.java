ed402532be00d379c0a970e32faf5c2466ad2eba
4ndres55-b <martin.hernandez.24.2006@gmail.com>
2026-06-09 01:12:07 -0400

    09-06-2026
    Se comprubea la compatibildad entre la api mascota y el postman

--- BackEnd/mascotas-service/mascotas-service/src/main/java/com/sanosysalvos/mascotas_service/service/MascotaService.java
+++ BackEnd/mascotas-service/mascotas-service/src/main/java/com/sanosysalvos/mascotas_service/service/MascotaService.java
@@ -9,12 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.sanosysalvos.mascotas_service.Repository.MascotaRepository;
-import com.sanosysalvos.mascotas_service.Repository.TipoRazaRepository;
-import com.sanosysalvos.mascotas_service.Repository.TipoMascotaRepository; // Importar nuevo repositorio
 import com.sanosysalvos.mascotas_service.dto.MascotaDatosDTO;
 import com.sanosysalvos.mascotas_service.model.Mascota;
 import com.sanosysalvos.mascotas_service.model.TipoRaza;
+import com.sanosysalvos.mascotas_service.repository.MascotaRepository;
+import com.sanosysalvos.mascotas_service.repository.TipoMascotaRepository;
+import com.sanosysalvos.mascotas_service.repository.TipoRazaRepository;
 import com.sanosysalvos.mascotas_service.model.TipoMascota; // Importar nuevo modelo
 import com.sanosysalvos.mascotas_service.model.EstadoMascota; // Importar Enum de Estado
 
