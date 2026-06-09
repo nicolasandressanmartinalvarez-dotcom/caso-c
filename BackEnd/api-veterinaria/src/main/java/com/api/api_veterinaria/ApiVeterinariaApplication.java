3ef3238be84b9bbbb4eb9671c357920f4ee72a42
Jmauriz559 <jmauriz559@gmail.com>
2026-06-08 22:01:02 -0400

    08-06
    creo la api-veterinaria y modifico el pom.xml y el aplication properties por compatibilidad

---
+++ BackEnd/api-veterinaria/src/main/java/com/api/api_veterinaria/ApiVeterinariaApplication.java
@@ -0,0 +1,13 @@
+package com.api.api_veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVeterinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVeterinariaApplication.class, args);
+	}
+
+}
