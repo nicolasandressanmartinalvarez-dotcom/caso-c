19dc2ec541bbeea05ce2182d7080f65d56c63986
4ndres55-b <martin.hernandez.24.2006@gmail.com>
2026-06-18 12:00:34 -0400

    se cambian las rutas protegidas

---
+++ FrontEnd/frontEnd_sanos_salvos/src/components/auth/RutaProtegidaAdmin.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+
+function ProtectedRoute({ children }) {
+    const { isAuthenticated, isLoading } = useAuth0();
+
+    if (isLoading) return <p>Cargando...</p>;
+
+    if (!isAuthenticated) {
+        return <p>No tienes acceso. Debes iniciar sesión.</p>;
+    }
+
+    return children;
+}
+
+export default ProtectedRoute;
