ca10b1497379300beac4000c4b55a1d350d49cbd
Jmauriz559 <jmauriz559@gmail.com>
2026-06-18 23:30:59 -0400

    18-06
    layaout de veterinaria avanzado

--- FrontEnd/frontEnd_sanos_salvos/src/features/veterinaria/layout/VeterinariaLayout.jsx
+++ FrontEnd/frontEnd_sanos_salvos/src/features/veterinaria/layout/VeterinariaLayout.jsx
@@ -1,17 +1,34 @@
 import VetLayout from "./VeterinariaLayout.module.css";
+import { useState } from "react";
 //import ListMasc from "../pages/listMasc/listMasc";
 //import ListMascPerd from "../pages/listMascPerd/listMascPerd";
 
 function VeterinariaLayout() {
+    const [vista, setVista] = useState("misMascotas");
     return (
         <section className={VetLayout["layout-veterinaria"]}>
             <aside className={VetLayout["sidebar-veterinaria"]}>
                 <h2>Panel Vet</h2>
 
-                <button>Mis Mascotas</button>
-                <button>Mascotas Perdidas</button>
-                <button>Mascotas Encontradas</button>
-                <button>Registrar Mascota</button>
+                <div className={VetLayout["menu-grupo"]}>
+                    <h3>Administrar Mascotas</h3>
+
+                    <ul>
+                        <li>Mis Mascotas</li>
+                        <li>Mascotas Perdidas</li>
+                        <li>Mascotas Encontradas</li>
+                        <li>Registrar Mascota</li>
+                    </ul>
+                </div>
+
+                <div className={VetLayout["menu-grupo"]}>
+                    <h3>Administración Veterinaria </h3>
+
+                    <ul>
+                        <li>Datos Veterinaria</li>
+                        <li>Contactos</li>
+                    </ul>
+                </div>
             </aside>
 
             <main className={VetLayout["contenido-veterinaria"]}>
