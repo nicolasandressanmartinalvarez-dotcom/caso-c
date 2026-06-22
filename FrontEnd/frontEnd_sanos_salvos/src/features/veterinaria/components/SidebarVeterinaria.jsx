02da6828e2fc3d39e25975489743c95963b0062d
Jmauriz559 <jmauriz559@gmail.com>
2026-06-22 04:15:26 -0400

    22-06
    Agrega vista de mascotas perdidas en veterinaria

--- FrontEnd/frontEnd_sanos_salvos/src/features/veterinaria/components/SidebarVeterinaria.jsx
+++ FrontEnd/frontEnd_sanos_salvos/src/features/veterinaria/components/SidebarVeterinaria.jsx
@@ -1,10 +1,26 @@
+import { NavLink } from "react-router-dom";
+
 function SidebarVeterinaria() {
     return (
         <aside>
             <ul>
-                <li>Inicio</li>
-                <li>Mascotas Perdidas</li>
-                <li>Veterinarias</li>
+                <li>
+                    <NavLink to="/veterinaria">
+                        Inicio
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink to="/veterinaria/perdidas">
+                        Mascotas Perdidas
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink to="/veterinaria">
+                        Veterinarias
+                    </NavLink>
+                </li>
             </ul>
         </aside>
     );
