613cd114c3de758fca74c208e3cf0ed1ecd82293
4ndres55-b <martin.hernandez.24.2006@gmail.com>
2026-06-23 18:58:20 -0400

    23-06-2026
    Se crea la API de usuarios

---
+++ BackEnd/usuarios_Permitidos/src/main/java/com/api_User/usuarios_Permitidos/UsuariosPermitidosApplication.java
@@ -0,0 +1,13 @@
+package com.api_User.usuarios_Permitidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosPermitidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosPermitidosApplication.class, args);
+	}
+
+}
---
+++ BackEnd/usuarios_Permitidos/src/test/java/com/api_User/usuarios_Permitidos/UsuariosPermitidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_User.usuarios_Permitidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosPermitidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
